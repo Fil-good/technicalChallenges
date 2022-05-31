a82e7c06058adef8be2ef01588db8109aa7059e6
Filip Brouwers <filip.brouwers@gmail.com>
2022-05-31 23:01:32 +0200

    layout

--- alten/roadPatches.java
+++ alten/roadPatches.java
@@ -1,14 +1,18 @@
 // write your code in Java SE 8
 
+// instruction: "..XX..X", where X = pithole, . = good road
+// road repair machine can fix 3 subsequent parts of the road
+// function needs to return the number of patches are needed to repair all pitholes
+
 class Solution {
-    public int solution(String S) {
-        int numberOfPatches = 0;
-        for(int i=0, n=S.length();i<n;i++) {
-            if(S.charAt(i)=='X') {
-                numberOfPatches += 1;
-                    i+=2;
-            }
+  public int solution(String S) {
+    int numberOfPatches = 0;
+    for(int i=0, n=S.length();i<n;i++) {
+        if(S.charAt(i)=='X') {
+            numberOfPatches += 1;
+                i+=2;
         }
-
-
-        return numberOfPatches;
+    }
+    return numberOfPatches;
+  }
+}
