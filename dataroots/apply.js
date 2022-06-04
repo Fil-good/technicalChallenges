8db3ebec60895c6f955fe9fc14e953d40016b1f3
Filip Brouwers <filip.brouwers@gmail.com>
2022-06-04 11:46:07 +0200

    fetch get

--- dataroots/apply.js
+++ dataroots/apply.js
@@ -1,40 +1,24 @@
 
 
-let ul = document.querySelector('ul')
 
-http://www.omdbapi.com/?t=lord
+async function fetchMovieTitles(url='') {
+
+  const response = await fetch(url)
+  return response.json();
+
+}
+
+let url = `http://www.omdbapi.com/?s=lord&apikey=65d6a47c`;
+let ul = document.querySelector('ul');
+let ulInnerHtml = "";
 
 
-fetch(`http://www.omdbapi.com/?t=lord&apikey=65d6a47c`)
-  .then(response => response.json())
+fetchMovieTitles(url)
   .then((data) => {
-    console.log(data.Title)
-    ul.innerHTML = `<li>${data.Title}</li>`
-
-
-  })
-
-
-// let results = document.querySelector("ul");
-// let query = "i=tt3896198";
-
-// const searchMovies = () => {
-//   fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=65d6a47c`)
-//     .then(response => response.json())
-//     .then((data) => {
-
-//       console.log(data)
-//       const title = `<li>${data.Title}</li>`
-//       // data.Search.forEach((result) => {
-//         // const movie = `<li class="list-inline-item">
-//         //   <img src="${result.Poster}" alt="">
-//         //   <p>${result.Title}</p>
-//         // </li>`
-//         console.log(`title: ${title}`)
-//         results.innerHTML += title
-//       })
-//     // })
-// }
-
-
-// http://www.omdbapi.com/?i=tt3896198&apikey=65d6a47c
+    data.Search.forEach(movie => {
+    let liUlInnerHtml = `<li>${movie.Title}</li>`;
+    ulInnerHtml += liUlInnerHtml;
+  })})
+  .then(() => {
+    console.log(ulInnerHtml);
+    ul.innerHTML = ulInnerHtml })
